fed41ef752daf511b1110c04b15dc40c9b2e75f8
Aseem Mangla <aseemmanglap@gmail.com>
2022-02-03 06:55:57 +0530

    rabbitmq-project-frontend

---
+++ rabbitmq-project-frontend/src/components/landingPage/LandingPage.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function LandingPage() {
+    const [isRegistered, setisRegistered] = useState(true);
+    
+  return <div></div>;
+}
+
+export default LandingPage;
